e872b52d5cb6d462b0207db8e61462dc318c84f2
Kim Kern <github@kimkern.de>
2018-04-11 22:01:31 +0200

    refactor ShareService

--- src/app/share.service.ts
+++ src/app/share.service.ts
@@ -1,36 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable()
 export class ShareService {
 
   navigator: any = navigator;
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private document) {
   }
 
-  isWebShareApiAvailable() {
+  isWebShareApiAvailable(): boolean {
     return  this.navigator && this.navigator.share;
   }
 
-  shareViaWebApi(title, text, url) {
+  shareViaWebApi(title, text, url): boolean {
     if (this.isWebShareApiAvailable()) {
       this.navigator.share({title, text, url});
+      return true;
+    } else {
+      return false;
     }
   }
 
-  copyToClipboard(text: string) {
-    const textArea = document.createElement('textarea');
-    textArea.textContent = text;
-    document.body.appendChild(textArea);
-    textArea.select();
+  copyToClipboard(text: string): boolean {
     let wasSuccessful;
-    try {
-      wasSuccessful = document.execCommand('copy');
-    } catch (error) {
-      console.warn('copy failed', error);
+    if (this.document) {
+      const textArea = this.document.createElement('textarea');
+      textArea.textContent = text;
+      this.document.body.appendChild(textArea);
+      textArea.select();
+      try {
+        wasSuccessful = this.document.execCommand('copy');
+      } catch (error) {
+        console.warn('copy failed', error);
+        wasSuccessful = false;
+      } finally {
+        this.document.body.removeChild(textArea);
+      }
+    } else {
+      console.error('#copyToClipboard: document not available');
       wasSuccessful = false;
-    } finally {
-      document.body.removeChild(textArea);
     }
     return wasSuccessful;
   }
